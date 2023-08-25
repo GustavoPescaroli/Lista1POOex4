783d3b36e9189758804658ca2c6a94f3deaee406
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:34:56 -0300

    Add files via upload

---
+++ Program (2).cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOx4
+{
+    internal class Tringulo
+    {
+        static void Main(string[] args)
+        {
+            Triangulo a;
+            a = new Triangulo ();
+
+
+            Console.WriteLine("Digite o valor da base");
+            a.setb(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("valor da altura");
+
+            a.seth(double.Parse(Console.ReadLine()));
+
+            a.calcular();
+
+
+            Console.WriteLine("O valor da area {0}", a.geta());
+        }
+    }
+}
